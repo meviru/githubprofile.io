0329c16cd4171533fe65c9669ace4527aeb39b61
Viral Patel <viralpatel508@gmail.com>
2024-06-25 10:47:52 +0530

    fix: added strict mode in main

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,10 @@ import GithubProvider from "./context/index.tsx";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <GithubProvider>
       <GlobalStyles />
       <AppLayout />
     </GithubProvider>
+  </React.StrictMode>
 );
